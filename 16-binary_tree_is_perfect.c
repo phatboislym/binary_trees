ade8406a888e2a24b0d85f66f87f60e78432e1e3
Adewole <phatboislym@protonmail.com>
2022-11-19 02:24:15 +0100

    add: task 16 skeleton

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect(const binary_tree_t *tree) - checks if a binary tree
+ * is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: Return 1 if perfect, 0 if tree is NULL or tree is not perfect
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree);
+{
+	if (!tree)
+		return (0);
+}
